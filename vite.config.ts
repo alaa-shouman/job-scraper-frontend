4ee9f7fc5c05042fa43ec120e6a210a6fe1fb46d
alaa <alaa.shouman@st.edu.ul.lb>
2026-02-26 22:21:14 +0200

    allow host

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: Number(process.env.PORT) || 4173,
+    allowedHosts: ["job-scraper-frontend.onrender.com"],
   },
   preview: {
     host: "0.0.0.0",
