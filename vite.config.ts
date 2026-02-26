72f43247291f617d2568111c05e8fd8abea3165a
alaa <alaa.shouman@st.edu.ul.lb>
2026-02-26 21:51:59 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  preview: {
+    host: "0.0.0.0",
+    port: parseInt(process.env.VITE_PORT || "4173", 10),
+  },
+});
